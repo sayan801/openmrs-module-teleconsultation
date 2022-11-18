2d096682b2b87f18d9dd5447605c1f089ed5b885
Kushboo <kushboojain.r@thoughtworks.com>
2022-11-18 12:31:30 +0530

    BAH-2408 | Adding authorizedd annotation

--- api/src/main/java/org/bahmni/module/teleconsultation/api/TeleconsultationService.java
+++ api/src/main/java/org/bahmni/module/teleconsultation/api/TeleconsultationService.java
@@ -1,9 +1,11 @@
 package org.bahmni.module.teleconsultation.api;
 
+import org.openmrs.annotation.Authorized;
 import org.openmrs.api.OpenmrsService;
 
 
 public interface TeleconsultationService extends OpenmrsService {
-	
+
+	@Authorized
 	public String generateTeleconsultationLink(String uuid);
 }
